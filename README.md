c144de65ec8813263cc213f424ad31f9ed7c9833
Pat Loughney <pattyloughney@gmail.com>
2022-02-26 01:07:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Setup
+
+First copy the configuration template to `botconfig.json`:
+
+``` 
+$ cp config-template.json botconfig.json
+```
+
+The bot will need a valid Discord access token which you can get by creating an
+application for the bot in the [Discord Developer Portal dashboard](devportal).
+Add a bot user to the application and find the token on the Bot page. Copy the
+token and add it to your `botconfig.json` file.
+
+[devportal]: https://discord.com/developers/applications/
+
+At this point you should be able to run the bot (assuming you have a Go
+toolchain installed):
+
+``` 
+$ go run .
+```
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/state"
+	"github.com/diamondburned/arikawa/v3/utils/json/option"
+)
+
+type BotConfig struct {
+	Token string
+}
+
+func execSlashCommand(cmd *discord.CommandInteraction, s *state.State, e *gateway.InteractionCreateEvent) {
+	switch cmd.Name {
+	case "ping":
+		data := api.InteractionResponse{
+			Type: api.MessageInteractionWithSource,
+			Data: &api.InteractionResponseData{
+				Content: option.NewNullableString("Pong!"),
+			},
+		}
+
+		if err := s.RespondInteraction(e.ID, e.Token, data); err != nil {
+			log.Println("failed to send interaction callback:", err)
+		}
+	default:
+		log.Printf("unknown command %#v %#v %#v", cmd, s, e)
+	}
+}
+
+func main() {
+	configFile, err := os.ReadFile("botconfig.json")
+	if err != nil {
+		log.Fatalln("failed to read configuration:", err)
+	}
+
+	var config BotConfig
+	if err := json.Unmarshal(configFile, &config); err != nil {
+		log.Fatalln("failed to parse configuration:", err)
+	}
+
+	s := state.New("Bot " + config.Token)
+
+	app, err := s.CurrentApplication()
+	if err != nil {
+		log.Fatalln("failed to get application ID:", err)
+	}
+
+	s.AddHandler(func(e *gateway.InteractionCreateEvent) {
+		switch v := e.Data.(type) {
+		case *discord.CommandInteraction:
+			execSlashCommand(v, s, e)
+		default:
+			log.Println("unknown event:", e)
+		}
+	})
+
+	s.AddIntents(gateway.IntentGuilds)
+	s.AddIntents(gateway.IntentGuildMessages)
+
+	if err := s.Open(context.Background()); err != nil {
+		log.Fatalln("failed to open:", err)
+	}
+	defer s.Close()
+
+	log.Println("Gateway connected. Registering commands.")
+
+	log.Println("Logging existing commands")
+	commands, err := s.Commands(app.ID)
+	if err != nil {
+		log.Fatalln("failed to get commands:", err)
+	}
+
+	for _, command := range commands {
+		log.Println("Existing command", command.Name, "found.")
+	}
+
+	newCommands := []api.CreateCommandData{
+		{
+			Name:        "ping",
+			Description: "Basic ping command.",
+		},
+	}
+
+	if _, err := s.BulkOverwriteCommands(app.ID, newCommands); err != nil {
+		log.Fatalln("failed to create command:", err)
+	}
+
+	// Block forever.
+	select {}
+}
